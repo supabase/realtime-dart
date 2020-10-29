ff10f79191f5ef56394e6019687e5ecab19d05d2
phamhieu <phamhieu1998@gmail.com>
2020-10-29 14:45:19 +0800

    chore: add example

---
+++ example/main.dart
@@ -0,0 +1,20 @@
+import 'package:realtime/realtime.dart';
+
+/// Example to use with Supabase Realtime https://supabase.io/
+void main() async {
+  var socket = Socket('ws://SUPABASE_API_ENDPOINT/realtime/v1',
+      params: {'apikey': 'SUPABSE_API_KEY'},
+      logger: (kind, msg, data) => {print('${kind} ${msg} ${data}')});
+  var channel = socket.channel('realtime:*');
+
+  socket.onMessage((message) {
+    print('MESSAGE ${message}');
+  });
+
+  socket.connect();
+  channel.subscribe();
+
+  // on unsubscribe and disconnect
+  // channel.unsubscribe();
+  // socket.disconnect();
+}
