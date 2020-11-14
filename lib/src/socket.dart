8eda83549c7353ade3bbfc61861cba1d5d71ca43
phamhieu <phamhieu1998@gmail.com>
2020-11-14 15:19:33 +0800

    fix: lint error

--- lib/src/socket.dart
+++ lib/src/socket.dart
@@ -165,25 +165,33 @@ class Socket {
     reconnectTimer.reset();
     if (heartbeatTimer != null) heartbeatTimer.cancel();
     heartbeatTimer = Timer.periodic(Duration(milliseconds: heartbeatIntervalMs), (Timer t) => sendHeartbeat());
-    stateChangeCallbacks['open'].forEach((callback) => callback());
+    for (final callback in stateChangeCallbacks['open']) {
+      callback();
+    }
   }
 
-  void onConnClose(event) {
+  void onConnClose(String event) {
     log('transport', 'close', event);
     triggerChanError();
     if (heartbeatTimer != null) heartbeatTimer.cancel();
     reconnectTimer.scheduleTimeout();
-    stateChangeCallbacks['close'].forEach((callback) => callback(event));
+    for (final callback in stateChangeCallbacks['close']) {
+      callback(event);
+    }
   }
 
-  void onConnError(error) {
+  void onConnError(dynamic error) {
     log('transport', error.toString());
     triggerChanError();
-    stateChangeCallbacks['error'].forEach((callback) => callback(error));
+    for (final callback in stateChangeCallbacks['error']) {
+      callback(error);
+    }
   }
 
   void triggerChanError() {
-    channels.forEach((channel) => channel.trigger(ChannelEvents.error.eventName()));
+    for (final channel in channels) {
+      channel.trigger(ChannelEvents.error.eventName());
+    }
   }
 
   String connectionState() {
@@ -218,13 +226,12 @@ class Socket {
   }
 
   void push({String topic, ChannelEvents event, dynamic payload, String ref}) {
-    final callback = () => {
-          encode({'topic': topic, 'event': event.eventName(), 'payload': payload, 'ref': ref}, (result) {
-            // print('send message $result');
-            conn.sink.add(result);
-          })
-        };
-
+    void callback() {
+      encode({'topic': topic, 'event': event.eventName(), 'payload': payload, 'ref': ref}, (result) {
+        // print('send message $result');
+        conn.sink.add(result);
+      });
+    }
     log('push', '$topic $event ($ref)', payload);
 
     if (isConnected()) {
@@ -262,7 +269,9 @@ class Socket {
 
   void flushSendBuffer() {
     if (isConnected() && sendBuffer.isNotEmpty) {
-      sendBuffer.forEach((callback) => callback());
+      for (final callback in sendBuffer){
+        callback();
+      }
       sendBuffer = [];
     }
   }
@@ -282,7 +291,9 @@ class Socket {
       channels
           .where((channel) => channel.isMember(topic))
           .forEach((channel) => channel.trigger(event, payload: payload, ref: ref));
-      stateChangeCallbacks['message'].forEach((callback) => callback(msg));
+      for (final callback in stateChangeCallbacks['message']) {
+        callback(msg);
+      }
     });
   }
 }
