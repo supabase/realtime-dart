86c599ce7c7046c51fb4e5e28c34defca9aa203f
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-07-07 09:36:56 +0900

    reverted sendJoin timeout time

--- test/channel_test.dart
+++ test/channel_test.dart
@@ -14,7 +14,7 @@ void main() {
   test('channel should be initially closed', () {
     final channel = RealtimeSubscription('topic', RealtimeClient('endpoint'));
     expect(channel.isClosed(), isTrue);
-    channel.sendJoin(const Duration(milliseconds: 5));
+    channel.sendJoin(const Duration(seconds: 5));
     expect(channel.isJoining(), isTrue);
   });
 
